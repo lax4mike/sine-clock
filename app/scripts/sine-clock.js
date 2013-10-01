43e0ad43ab40fa63fc7b36a39161fcd97068d48d
Mike Lambert <mlambert@onepica.com>
2013-10-01 14:57:04 -0400

    Using position absolute and left with 2 svgs

--- app/scripts/sine-clock.js
+++ app/scripts/sine-clock.js
@@ -4,8 +4,8 @@ $(function(){
 	Raphael.fn.drawGraph = function() {
 		var set = this.set();
 
-		var h = paper.height;
-		var w = paper.width;
+		var h = this.height;
+		var w = this.width;
 
 		var p = [
 		    // ["M", 0, h/2],
@@ -23,8 +23,8 @@ $(function(){
 
 		var set = this.set();
 
-		var h = paper.height;
-		var w = paper.width;
+		var h = this.height;
+		var w = this.width;
 
 		var p = [
 		    ["M", 0, h/2],
@@ -44,14 +44,13 @@ $(function(){
 		var set = this.set();
 
 		var stroke = 5;
-		var hpadding = 5;
-		var w = paper.width - (hpadding*2);
-		var h = paper.height;
+		var hpadding = 0;
+		var w = this.width - (hpadding*2);
+		var h = this.height;
 
 		var p = [];
 
 		// for each x in the width of the svg, calculate y of sin wave
-		// skip every 10 pixels, less calculating
 		for(var x = (0); x <= (w); x = x+10){
 
 			var radians = (x + (w/2)) * (Math.PI/(w/2));
@@ -64,17 +63,39 @@ $(function(){
 
 		}
 
-		set.push(this.path(p).attr({"stroke": "blue", "stroke-width": stroke, "stroke-linecap": "round"}));
+		set.push(this.path(p).attr({"stroke": "blue", "stroke-width": stroke, "stroke-linecap": "butt"}));
 
 		return set;
 	}
 
-	// create svg
-	var paper = Raphael(0, 0, 500, 250);
-	$('svg').css({'position': 'static'}); // hack to make the svn staticly posisioned
+
+	var face = {width: 500, height: 250};
+
+	// create svgs and clock-face
+	$("<div></div>", { 
+		id: "clock-face", 
+		css: {
+			position: "relative",
+			width: face.width, 
+			height: face.height,
+			overflow: "hidden"
+		}
+		
+	}).appendTo('.container');
+
+	var sine1Raphael = Raphael("clock-face", face.width, face.height);
+	var sine2Raphael = Raphael("clock-face", face.width, face.height);
+	var sine1 = $(sine1Raphael.canvas);
+	var sine2 = $(sine2Raphael.canvas);
+	$(sine1, sine2).css({'position': 'absolute'});
+	sine1Raphael.drawSine(Math.PI*3/2);
+	sine2Raphael.drawSine(Math.PI*3/2);
+	$(sine1).css({'left': 0});
+	$(sine2).css({'left': face.width});
 
 	// number to be displayed on the front end
 	var displaySeconds = 0; 
+	var second = sine2;
 
 	setInterval(function(){
 		
@@ -85,19 +106,19 @@ $(function(){
 		var seconds = d.getSeconds();
 		var milliSeconds = seconds + (d.getMilliseconds() / 1000);
 		var milliSecondsRadians = -milliSeconds * (Math.PI / 30) + (Math.PI / 2);
-		
-		// redraw the sine wave starting at the milliSecondsRadians
-		paper.clear();
-		paper.drawSine(milliSecondsRadians);
-		paper.drawGraph();
 
+		var left = -(milliSeconds/60) * face.width;
+
+		$(sine1).css({left: left + face.width});
+		$(sine2).css({left: left});
+		
 		// update the frontend seconds if needed
 		if (displaySeconds != seconds){
 			displaySeconds = seconds;
 			$('#seconds').html(displaySeconds)
 		}
 
-	}, 100);
+	}, 10);
 
 
 
